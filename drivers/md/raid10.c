121981f01defe45c60de6d18ab62ca1d46701863
Xiao Ni <xni@redhat.com>
2025-03-04 17:09:50 +0800

    md/raid10: Don't print warn calltrace for discard with REQ_NOWAIT
    There is no need to print warn call trace. And it also can confuse
    qe and mark test case failure.
    
    Signed-off-by: Xiao Ni <xni@redhat.com>

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -1631,7 +1631,7 @@ static int raid10_handle_discard(struct mddev *mddev, struct bio *bio)
 	if (test_bit(MD_RECOVERY_RESHAPE, &mddev->recovery))
 		return -EAGAIN;
 
-	if (WARN_ON_ONCE(bio->bi_opf & REQ_NOWAIT)) {
+	if (bio->bi_opf & REQ_NOWAIT) {
 		bio_wouldblock_error(bio);
 		return 0;
 	}
