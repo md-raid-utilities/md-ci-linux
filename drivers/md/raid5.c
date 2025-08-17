d9185fe7c3f82e8115327f88a0817b79b01017dc
Coly Li <colyli@kernel.org>
2025-08-17 23:26:44 +0800

    block: ignore underlying non-stack devices io_opt
    This patch adds a new BLK_FLAG_STACK_IO_OPT for stack block device. If a
    stack block device like md raid5 declares its io_opt when don't want
    blk_stack_limits() to change it with io_opt of underlying non-stack
    block devices, BLK_FLAG_STACK_IO_OPT can be set on limits.flags. Then in
    blk_stack_limits(), lcm_not_zero(t->io_opt, b->io_opt) will be avoided.
    
    For md raid5, it is necessary to keep a proper io_opt size for better
    I/O thoughput.
    
    Signed-off-by: Coly Li <colyli@kernel.org>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -7730,6 +7730,7 @@ static int raid5_set_limits(struct mddev *mddev)
 	lim.io_min = mddev->chunk_sectors << 9;
 	lim.io_opt = lim.io_min * (conf->raid_disks - conf->max_degraded);
 	lim.features |= BLK_FEAT_RAID_PARTIAL_STRIPES_EXPENSIVE;
+	lim.flags |= BLK_FLAG_STACK_IO_OPT;
 	lim.discard_granularity = stripe;
 	lim.max_write_zeroes_sectors = 0;
 	mddev_stack_rdev_limits(mddev, &lim, 0);
