9f88d97159e879f64c84c468564cac9ab21bd02e
Yu Kuai <yukuai3@huawei.com>
2025-08-28 14:57:30 +0800

    blk-crypto: convert to use bio_submit_split_bioset()
    On the one hand unify bio split code, prepare to fix disordered split
    IO; On the other hand fix missing blkcg_bio_issue_init() and
    trace_block_split() for split IO.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- block/blk-crypto-fallback.c
+++ block/blk-crypto-fallback.c
@@ -223,17 +223,12 @@ static bool blk_crypto_fallback_split_bio_if_needed(struct bio **bio_ptr)
 			break;
 	}
 	if (num_sectors < bio_sectors(bio)) {
-		struct bio *split_bio;
-
-		split_bio = bio_split(bio, num_sectors, GFP_NOIO,
-				      &crypto_bio_split);
-		if (IS_ERR(split_bio)) {
-			bio->bi_status = BLK_STS_RESOURCE;
+		bio = bio_submit_split_bioset(bio, num_sectors,
+					      &crypto_bio_split);
+		if (!bio)
 			return false;
-		}
-		bio_chain(split_bio, bio);
-		submit_bio_noacct(bio);
-		*bio_ptr = split_bio;
+
+		*bio_ptr = bio;
 	}
 
 	return true;
