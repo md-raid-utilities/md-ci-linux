da97ee29f5ac9892f41bddcdefb17236d5311c49
Kenta Akagi <k@mgml.me>
2025-09-15 12:42:09 +0900

    md/raid1,raid10: Add error message when setting MD_BROKEN
    Once MD_BROKEN is set on an array, no further writes can be
    performed to it.
    The user must be informed that the array cannot continue operation.
    
    Signed-off-by: Kenta Akagi <k@mgml.me>

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -2039,6 +2039,10 @@ static void raid10_error(struct mddev *mddev, struct md_rdev *rdev)
 
 	if (test_bit(In_sync, &rdev->flags) && !enough(conf, rdev->raid_disk)) {
 		set_bit(MD_BROKEN, &mddev->flags);
+		pr_crit("md/raid10:%s: Disk failure on %pg, this is the last device.\n"
+			"md/raid10:%s: Cannot continue operation (%d/%d failed).\n",
+			mdname(mddev), rdev->bdev,
+			mdname(mddev), mddev->degraded + 1, conf->geo.raid_disks);
 
 		if (!mddev->fail_last_dev) {
 			spin_unlock_irqrestore(&conf->device_lock, flags);
